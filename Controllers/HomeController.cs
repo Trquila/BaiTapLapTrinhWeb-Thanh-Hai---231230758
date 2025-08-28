a2e07ed9b2b599706df76c257c7c02136e73d047
Trquila <thanhhaidz060905@gmail.com>
2025-08-28 19:17:56 +0700

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ThanhHai231230758.Models;
+
+namespace ThanhHai231230758.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/NewController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThanhHai231230758.Controllers
+{
+    public class NewController1 : Controller
+    {
+        public IActionResult Action1()
+        {
+            return View();
+        }
+        public IActionResult Action2()
+        {
+            return View();
+        }
+    }
+}
