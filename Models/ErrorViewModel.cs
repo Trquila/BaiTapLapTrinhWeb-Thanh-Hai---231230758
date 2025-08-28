e40548637f925c1b514ac44e8c848814bdf7449b
Trquila <thanhhaidz060905@gmail.com>
2025-08-28 19:24:15 +0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThanhHai231230758.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
